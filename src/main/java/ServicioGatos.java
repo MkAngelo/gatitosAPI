3e0ab91e5d8b44da15ca15708342981b7040ec9b
MkAngelo <mikequi000@gmail.com>
2022-02-04 13:03:35 -0600

    Listando datos de la API

--- src/main/java/ServicioGatos.java
+++ src/main/java/ServicioGatos.java
@@ -1,3 +1,9 @@
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.io.IOException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,7 +14,18 @@
  * @author Mike
  */
 public class ServicioGatos {
-    public static void verGatos(){
+    public static void verGatos() throws IOException{
+        // Traer datos de la API
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").method("GET", null).build();
+        Response response = client.newCall(request).execute();
         
+        String elJson = response.body().string();
+        
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0, elJson.length()-1);
+        
+        Gson gson = new Gson();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
     }
 }
